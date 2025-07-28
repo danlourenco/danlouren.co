fcc005c0ce7ef803b85d041a575e3588fdb3bebe
Dan Lourenco <danlourenco@gmail.com>
2025-07-28 13:47:21 -0400

    Refactor Header component links and update SITE_TITLE for clarity

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "danlouren.co | Dan Lourenço's personal website";
+export const SITE_TITLE = "danlouren.co";
 export const SITE_DESCRIPTION = "Welcome to my website!";
